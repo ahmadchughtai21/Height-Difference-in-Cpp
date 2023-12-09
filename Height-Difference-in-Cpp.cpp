1f2232d4bc554bc349cb76ddc9375f1b04e2d7d4
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2023-12-09 18:01:17 +0500

    Initial commit

---
+++ Height-Difference-in-Cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const float A = 8*12;
+    float B;
+    
+    cout << "Friends Height in inches : " ;
+    
+    cin >> B;
+
+    cout << "The difference of hieght in Inches : " << A-B <<endl ;
+
+
+    return 0;
+	
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Height-Difference-in-Cpp
+ C++ program calculates height difference in inches, taking user input for a friend's height.
